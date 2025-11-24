750781f4cf5be182abc767b51adef4568220f21e
EGladki <e.gladki@gmail.com>
2025-11-24 16:52:43 +0300

    ref: refactor SecurityConfig, add test controller for Minio

---
+++ src/main/java/com/gladkiei/cloudstorage/config/SessionConfig.java
@@ -0,0 +1,23 @@
+package com.gladkiei.cloudstorage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.session.web.http.CookieHttpSessionIdResolver;
+
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public CookieHttpSessionIdResolver cookieHttpSessionIdResolver() {
+        return new CookieHttpSessionIdResolver();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
+    }
+
+}
---
+++ src/main/java/com/gladkiei/cloudstorage/controllers/MinioTestController.java
@@ -0,0 +1,75 @@
+package com.gladkiei.cloudstorage.controllers;
+
+import io.minio.*;
+import io.minio.errors.*;
+import io.swagger.v3.oas.annotations.Operation;
+import org.hibernate.Remove;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequestMapping("/api")
+public class MinioTestController {
+
+    @Autowired
+    private MinioClient minioClient;
+
+    @Value("${minio.root-bucket}")
+    private String root;
+
+    @PostMapping("/upload-file")
+    @Operation(summary = "test upload")
+    public void upload() throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        File file = new File("C:\\Dev\\data\\text.txt");
+        StringBuilder builder = new StringBuilder();
+        builder.append("From ");
+        builder.append("builder");
+        byte[] data = builder.toString().getBytes(StandardCharsets.UTF_8);
+
+        minioClient.uploadObject(UploadObjectArgs.builder()
+                .bucket("user-files")
+                .object("text.txt")
+                .filename("C:\\Dev\\data\\text.txt")
+                .build());
+    }
+
+    @PostMapping("/delete-file")
+    @Operation(summary = "test delete")
+    public void delete() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+        minioClient.removeObject(RemoveObjectArgs.builder()
+                .bucket("user-files")
+                .object("text.txt")
+                .build());
+    }
+
+    @PostMapping("/create-bucket")
+    @Operation(summary = "create bucket")
+    public void createBucket(@RequestBody String name) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        minioClient.makeBucket(MakeBucketArgs.builder()
+                .bucket(root + name)
+                .build());
+
+    }
+
+    @PostMapping("/delete-bucket")
+    @Operation(summary = "delete bucket")
+    public void deleteBucket(@RequestBody String name) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        minioClient.removeBucket(RemoveBucketArgs.builder()
+                .bucket(root + name)
+                .build());
+
+    }
+
+}
